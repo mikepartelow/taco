97cac11b740ef263c100d4318c3b3d0849e193d8
Mike Partelow <mikep@shoehater.com>
2013-07-27 10:54:04 -0700

    next steps

--- lib/taco.rb
+++ lib/taco.rb
@@ -91,12 +91,37 @@ module Schema
     base.extend(ClassMethods)
   end
   
+  def valid?
+    self.class.instance_variable_get("@schema_attrs").each do |attr, opts|
+      if opts[:values]
+        value = eval(attr.to_s)
+        if opts[:values].is_a?(Array)
+          return false unless opts[:values].include? value
+        elsif opts[:values].is_a?(Proc)
+          return false unless opts[:values].call(value)
+        end
+      end
+    end
+    
+    true
+  end
+  
   module ClassMethods
     def schema_attr(name, opts)      
       @schema_attrs ||= {}
 
       raise TypeError.new("attribute #{name}: missing or invalid :class") unless opts[:class].is_a?(Class)
       raise TypeError.new("attribute #{name}: missing or invalid :default") unless opts[:default].is_a?(opts[:class])
+
+      if opts[:values]
+        unless opts[:values].is_a?(Array) || opts[:values].is_a?(Proc)
+          raise ArgumentError.new("attribute #{name}: expecting Array or Proc for :values")
+        end
+        
+        if opts[:values].is_a?(Array)
+          raise TypeError.new("attribute #{name}: wrong type in :values Array") unless opts[:values].all? { |v| v.is_a?(opts[:class]) }
+        end
+      end
 
       raise ArgumentError.new("attribute #{name}: already exists") if @schema_attrs[name]
       
--- spec/schema_spec.rb
+++ spec/schema_spec.rb
@@ -13,8 +13,9 @@ describe Schema do
         include Schema
         
         schema_attr :bar, class: String, default: 'abc123'
-        schema_attr :baz, class: String, default: '', settable: true
-        schema_attr :ick, class: Fixnum, default: 1, settable: true
+        schema_attr :baz, class: String, default: 'x', settable: true, values: lambda { |v| v !~ /^\s*$/ }
+        schema_attr :ick, class: Fixnum, default: 1, settable: true, values: [1,2,3,4,5]
+        schema_attr :thud, class: String, default: 'a', settable: true, values: %w(a b c)
       end
     end
     
@@ -106,21 +107,87 @@ describe Schema do
 
           schema_attr :baz, class: String, default: 'baz'
         end         
-      end        
-           
+      end    
+            
+      it "allows a properly typed Array for :values" do
+        class Bar
+          include Schema
+
+          schema_attr :baz, class: String, default: 'baz', values: ['a', 'b', 'c']
+        end 
+        
+        expect {
+          class Baz
+            include Schema
+
+            schema_attr :baz, class: String, default: 'baz', values: [1, 2, 3]
+          end           
+        }.to raise_error(TypeError)
+      end
+      
+      it "allows a Proc for :values" do
+        class Bar
+          include Schema
+
+          schema_attr :baz, class: String, default: 'baz', values: lambda { |x| true }
+        end 
+      end
+      
+      it "does not allow other types for :values" do
+        expect {
+          class Bar
+            include Schema
+
+            schema_attr :baz, class: String, default: 'baz', values: 2
+          end           
+        }.to raise_error(ArgumentError)
+      end
     end
     
-    describe "setter validations" do
-      # baz is a String
-      specify { expect { Foo.new.baz = nil }.to raise_error(TypeError) }
-      specify { expect { Foo.new.baz = 123 }.to raise_error(TypeError) }
-      specify { expect { Foo.new.baz = Time.new }.to raise_error(TypeError) }
+    describe "instance variable validations" do
+      describe "type checking" do
+        # baz is a String
+        specify { expect { Foo.new.baz = nil }.to raise_error(TypeError) }
+        specify { expect { Foo.new.baz = 123 }.to raise_error(TypeError) }
+        specify { expect { Foo.new.baz = Time.new }.to raise_error(TypeError) }
 
-      # ick is a Fixnum
-      specify { expect { Foo.new.ick = nil }.to raise_error(TypeError) }
-      specify { expect { Foo.new.ick = '123' }.to raise_error(TypeError) }
-      specify { expect { Foo.new.ick = Time.new }.to raise_error(TypeError) }
+        # ick is a Fixnum
+        specify { expect { Foo.new.ick = nil }.to raise_error(TypeError) }
+        specify { expect { Foo.new.ick = '123' }.to raise_error(TypeError) }
+        specify { expect { Foo.new.ick = Time.new }.to raise_error(TypeError) }
+      end
       
+      describe "valid?" do
+        let(:foo) { Foo.new }
+        
+        it "is valid by default" do
+          p foo.instance_variables
+          foo.should be_valid
+        end
+        
+        describe "disallowed values" do
+          specify { foo.baz = ''; foo.should_not be_valid }          
+          specify { foo.baz = '     '; foo.should_not be_valid }
+          specify { foo.baz = "\n"; foo.should_not be_valid }
+          specify { foo.ick = 77; foo.should_not be_valid }
+          specify { foo.thud = 'd'; foo.should_not be_valid }
+        end
+        
+        describe "allowed values" do
+          specify { foo.baz = 'hello world'; foo.should be_valid }          
+          specify { foo.ick = 3; foo.should be_valid }          
+          specify { foo.thud = 'c'; foo.should be_valid }
+        end        
+      end
+      
+      describe "automatic coercion" do
+        it "should coerce String to Time"        
+        it "should coerce String to Fixnum"
+        it "should truncate subsec precision of Time"
+        it "should strip whitespace from String"
+        it "should raise [X] when failing to coerce a String to Time"
+        it "should raise [X] when failing to coerce a String to Fixnum"
+      end
     end
     
   end
